7120fa2c9529e7e2c0b205c6e891d75996135afc
Cody Ebert <cebert@uoregon.edu>
2015-12-29 17:52:28 -0800

    Added basic screen, began building game engine, and main for our game

---
+++ TheGame/TheGame/src/MainGame.java
@@ -0,0 +1,22 @@
+import javax.swing.JApplet;
+import javax.swing.JSpinner;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+
+public class MainGame extends JApplet {
+
+	/**
+	 * Create the applet.
+	 */
+	public MainGame() {
+		
+		JLabel lblNewLabel = new JLabel("                                Legend of the Seven Pillars");
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+
+	}
+
+}

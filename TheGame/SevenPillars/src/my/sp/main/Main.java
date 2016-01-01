daaaf604284e8427ed7de12c19f5429bad654ade
Cody Ebert <cebert@uoregon.edu>
2016-01-01 14:37:45 -0800

    Fixed full screen mode
    12

--- TheGame/SevenPillars/src/my/sp/main/Main.java
+++ TheGame/SevenPillars/src/my/sp/main/Main.java
@@ -1,20 +1,25 @@
 package my.sp.main;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import my.sp.gameloop.GameLoop;
 import the.game.gop.main.GameWindow;
 import the.game.gop.main.SpriteSheet;
 
 public class Main {
-	//static SpriteSheet blocks = new SpriteSheet();
-	
+	 	
+	public static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	public static int width = gd.getDisplayMode().getWidth();
+	public static int height = gd.getDisplayMode().getHeight();
+
 	public static void main(String[] args) {
 		
-		
 	
-		GameWindow frame = new GameWindow("The Seven Pillars", 1280, 720);
+		GameWindow frame = new GameWindow("The Seven Pillars", width, height);
 		//Full screen is broken
 		frame.setFullscreen(1);
-		frame.add(new GameLoop(1280, 720));
+		frame.add(new GameLoop(width, height));
 		
 		frame.setVisible(true);
 		
